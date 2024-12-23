1208e162560c000f19178bf51d35b844cc6f3ad4
Daniel Sá <pg56120@alunos.uminho.pt>
2024-12-23 14:05:36 +0000

    Parte dos medicos

---
+++ Medico/TratamentoPacientes.py
@@ -0,0 +1,15 @@
+#Classe para a mensagem que informa o Medico qual o Paciente e sintomas a tratar
+
+class TratamentoPacientes:
+    def __init__(self, jid: str, sintomas: list):
+        self.jid = jid
+        self.sintomas = sintomas
+
+    def getPaciente(self):
+        return self.jid
+
+    def getSintomas(self):
+        return self.sintomas
+
+    def __str__(self):
+        return f' {self.jid} {self.sintomas}'
